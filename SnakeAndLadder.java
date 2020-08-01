a830f36c679588d777c54db71508fe41802c86ac
nnalin04 <nnalin04@gmail.com>
2020-08-01 11:14:47 +0530

    added: comments to the function

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -6,6 +6,7 @@ class SnakeAndLadder {
         playingAGame(1);
     }
 
+    //function to start and play the game.
     public static void playingAGame(int numOfPlayers) {
 
         int[] playersPositions = new int[numOfPlayers];
@@ -30,6 +31,7 @@ class SnakeAndLadder {
 
     }
 
+    //function to decide the players moving position.
     public static int[] playingARound(int[] playersPositions) {
         for (int i = 0; i < playersPositions.length; i++) {
             playersPositions[i] = movingPosition(playersPositions[i]);
@@ -60,6 +62,7 @@ class SnakeAndLadder {
         return r.nextInt(3) + 1;
     }
 
+    //function ot decide the type of move.
     public static int movingPosition(int playerPosition) {
         int rolledDie = rollTheDie();
 
