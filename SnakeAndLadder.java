8e096cdc9efc8b1b148778fc13a2bcda42fab039
nnalin04 <nnalin04@gmail.com>
2020-08-01 11:56:56 +0530

    refactored: removing the printing function

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -3,7 +3,7 @@ import java.util.Random;
 class SnakeAndLadder {
 
     public static void main(String[] args) {
-        playingARound(3);
+        playingARound(1);
     }
 
     //function to decide the players moving position.
