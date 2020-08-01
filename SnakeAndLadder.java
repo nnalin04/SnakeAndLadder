8847b812c9635ad7a3db07b17054e2d11c719d45
nnalin04 <nnalin04@gmail.com>
2020-08-01 11:23:43 +0530

    added: comments to the function

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -6,6 +6,7 @@ class SnakeAndLadder {
         playingAGame(1);
     }
 
+    //function to start and play the game.
     public static void playingAGame(int numOfPlayers) {
 
         int[] playersPositions = new int[numOfPlayers];
@@ -34,6 +35,7 @@ class SnakeAndLadder {
 
     }
 
+    //function to print the current position of the player.
     public static void currentPositions(int[] playersPositions, int numOfRounds) {
         for (int i = 0; i < playersPositions.length; i++) {
             System.out.println("player" + (i + 1) + " after " + numOfRounds + " dice played the current position is: "
@@ -42,6 +44,7 @@ class SnakeAndLadder {
         System.out.println();
     }
 
+    //function to decide the players moving position.
     public static int[] playingARound(int[] playersPositions) {
         int currentPosition = 0;
         for (int i = 0; i < playersPositions.length; i++) {
@@ -77,6 +80,7 @@ class SnakeAndLadder {
         return r.nextInt(3) + 1;
     }
 
+    //function ot decide the type of move.
     public static int movingPosition(int playerPosition) {
         int rolledDie = rollTheDie();
 
