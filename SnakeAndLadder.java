cfe902db3ec9f3c8c02b12d9ada108a54c557594
nnalin04 <nnalin04@gmail.com>
2020-08-01 11:52:09 +0530

    refactored: removing the printing function

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,6 +1,6 @@
 class SnakeAndLadder {
     public static void main(String[] args) {
-        playingARound(3);
+        playingARound(1);
     }
 
     //function to play the game.
@@ -11,16 +11,6 @@ class SnakeAndLadder {
         // calling a function to set the starting position as 0.
         settingStartingPositions(playersPositions);
 
-        // displaying players current position
-        currentPositions(playersPositions);
-
-    }
-
-    // function to print the player current position.
-    public static void currentPositions(int[] playersPositions) {
-        for (int i = 0; i < playersPositions.length; i++) {
-            System.out.println("player" + (i + 1) + " current position is: " + playersPositions[i]);
-        }
     }
 
     // function to set players starting position as zero.
